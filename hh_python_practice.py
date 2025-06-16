dfd1177d46ad31f071f51fbd63dfb825945f3f44
Kloshar <kloshar13@yahoo.com>
2025-06-16 22:01:43 +0300

    homework

--- hh_python_practice.py
+++ hh_python_practice.py
@@ -1,21 +1,22 @@
+# coding=windows-1251 
 def check_candidates (names_input: str, scores_input: str)->str:
     names = names_input.split(',')
     scores = scores_input.split('|')
     sum = 0
     count = 0
+    all_scores = []
     for lst in scores:
+        sumOne = 0
         for n in lst.split(','):
             print(n)
             sum += int(n)
+            sumOne += int(n)
             count += 1
-    
-            
-    print(f"sum={sum}")
-    print(f"count={count}")
-    average = sum / count            
-    print(f"average={average}")
+        print(f"Длина списка: {len(lst)}")
+        all_scores.append(sumOne / len(lst))
+    average = sum / count
 
-
+    print(f"all_scores={all_scores}")
     
 
 #names_input = input()

c30cec4bd92ca3620774b54bbad77ed081fffe04
Kloshar <kloshar13@yahoo.com>
2025-07-27 10:11:59 +0300

    homework 1

---
+++ easy05.py
@@ -0,0 +1,19 @@
+# coding=windows-1251
+def filter_unique_words(answer_list:str)->str:
+    allWords = answer_list.split()
+    filtered = ""
+
+    for w in allWords:
+        if allWords.count(w) == 1:
+            filtered += w + " "
+
+    if(filtered == ""): filtered = "-1"
+
+    return filtered.strip()
+
+#answer_list = input()
+answer_list = "python java c++ python"
+#answer_list = "красный оранжевый жёлтый зелёный"
+#answer_list = "ой ой ой"
+unique_words = filter_unique_words(answer_list)
+print(unique_words)

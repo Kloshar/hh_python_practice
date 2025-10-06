29d05104edc0cac78ea0c3bb54f9a36a60ef7ff8
Kloshar <kloshar13@yahoo.com>
2025-10-06 12:13:54 +0300

    middle6 finished

--- middle6.py
+++ middle6.py
@@ -5,22 +5,21 @@ class TextProcessor:
     def __init__(self, text):
         self.text = text
 
-    def extract_emails(self):
-        
-        pattern1 = re.compile(r'[a-z1-9]+@[a-z1-9]+\.net|com', flags=re.IGNORECASE)
-
+    def extract_emails(self):        
+        pattern1 = re.compile(r'[a-z1-9_\.-]+@[a-z1-9_\.-]+\.(?:ru|com|org|net)', flags=re.IGNORECASE)
         result = re.findall(pattern1, self.text)
 
-        print(result)
-
-        #print("Не найдено")
+        if len(result) > 0:
+            for adress in result:
+                print(adress)
+        else:
+            print("Не найдено")
 
 #input_text = input()
-
 input_text = "Это текст с email user123@example.com и user456@mail.ru, но не example.com"
-# input_textxt = "Это пример текста с невалидными адресами: userQ, Qdomain.com"
-# input_textxt = "Это текст с email 123@example.com и некоторыми другими данными."
-# input_textxt = "Текст без email-адресов"
+input_text = "Это пример текста с невалидными адресами: userQ, Qdomain.com"
+input_text = "Это текст с email 123@example.com и некоторыми другими данными."
+input_text = "Текст без email-адресов"
 
 processor = TextProcessor(input_text)
 processor.extract_emails()
